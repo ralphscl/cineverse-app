622eacc94de386a0e1c06c1d71df12797490ae65
Ralph Spencer Lacson <ralphlacs308@gmail.com>
2024-03-01 05:48:46 +0800

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Row from './components/Row'
+import './App.css'
+
+import { requests, getTvShows } from './service/requests';
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/service/requests.js
@@ -0,0 +1,13 @@
+import networkId from './networks.js';
+
+const ACCESS_TOKEN = '957c783e9cebad9b1f8f8c5cb4dbf044';
+
+export const requests = {
+  'getTrending': `/trending/tv/week?language=en-US`,
+  'getPopular': `/tv/popular?language=en-US&page=1`,
+  'getTopRated': `/tv/top_rated?language=en-US&page=1`,
+};
+
+export const getTvShows = ( page = 1, network, genre ) => {
+  return `/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_average.desc&with_networks=${networkId[network]}`;
+}
