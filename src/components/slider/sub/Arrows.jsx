264826b9504ecdd921ab50bbbba6d986c36e9c6e
Ralph Spencer Lacson <ralphlacs308@gmail.com>
2024-04-21 18:44:15 +0800

    Sldier controls hover animation

--- src/components/slider/sub/Arrows.jsx
+++ src/components/slider/sub/Arrows.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./Arrows.css";
 
 const Arrows = ({ prevSlide, nextSlide }) => {
   return (
-    <div className="arrows">
-      <div className="left-arrow" onClick={prevSlide}>
+    <motion.div className="arrows">
+      <motion.div
+        className="left-arrow"
+        onClick={prevSlide}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ translateX: -5 }}
+        transition={{ type: "spring", stiffness: 500 }}
+      >
         {"<"}
-      </div>
-      <div className="right-arrow" onClick={nextSlide}>
+      </motion.div>
+      <motion.div
+        className="right-arrow"
+        onClick={nextSlide}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ translateX: 5 }}
+        transition={{ type: "spring", stiffness: 500 }}
+      >
         {">"}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
